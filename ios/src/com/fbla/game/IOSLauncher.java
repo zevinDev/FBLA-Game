cf480cea84d62b63d26b0c5b62db911e6ef845bc
Zevin Burzynski <86094612+REALziez@users.noreply.github.com>
2023-08-31 17:48:19 +0000

    (fix) unnecessary files

--- ios/src/com/fbla/game/IOSLauncher.java
+++ ios/src/com/fbla/game/IOSLauncher.java
@@ -5,7 +5,7 @@ import org.robovm.apple.uikit.UIApplication;
 
 import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
 import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
-import com.fbla.game.FBLA;
+//import com.fbla.game.FBLA;
 
 public class IOSLauncher extends IOSApplication.Delegate {
     @Override

c39aa2e2b5f5a4d79f3abbf47aaa295d396844c9
Zevin Burzynski <86094612+REALziez@users.noreply.github.com>
2023-09-13 01:48:59 +0000

    (feat) player and animations

--- core/src/com/fbla/game/GameScreen.java
+++ core/src/com/fbla/game/GameScreen.java
@@ -1,66 +1,143 @@
 package com.fbla.game;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.Input.Keys;
+
 
 public class GameScreen extends ScreenAdapter {
 
     FBLA game;
 
-    float circleX = 300;
-    float circleY = 150;
-    float circleRadius = 50;
 
-    float xSpeed = 4;
-    float ySpeed = 3;
+
+	// Objects used
+	Animation<TextureRegion> downAnimation; // Must declare frame type (TextureRegion)
+	Animation<TextureRegion> idleAnimation; // Must declare frame type (TextureRegion)
+	Animation<TextureRegion> upAnimation; // Must declare frame type (TextureRegion)
+	Animation<TextureRegion> leftAnimation; // Must declare frame type (TextureRegion)
+	Animation<TextureRegion> rightAnimation; // Must declare frame type (TextureRegion)
+	Texture spriteSheet;
+	SpriteBatch spriteBatch;
+	TextureRegion currentFrame;
+
+	// A variable for tracking elapsed time for the animation
+	float stateTime;
+
+	// Character X and Y coords
+	float playerX = 100;
+	float playerY = 100;
+	float playerSpeed = 150;
+	boolean playerIdle = true;
+	float playerIdleFrame = 0;
 
     public GameScreen(FBLA game) {
         this.game = game;
     }
 
+
     @Override
     public void show() {
-        Gdx.input.setInputProcessor(new InputAdapter() {
-            @Override
-            public boolean touchDown(int x, int y, int pointer, int button) {
-                int renderY = Gdx.graphics.getHeight() - y;
-                if (Vector2.dst(circleX, circleY, x, renderY) < circleRadius) {
-                    game.setScreen(new EndScreen(game));
-                }
-                return true;
-            }
-        });
+        // Load the sprite sheet as a Texture
+		spriteSheet = new Texture(Gdx.files.internal("tyler.png"));
+
+		// Use the split utility method to create a 2D array of TextureRegions. This is
+		// possible because this sprite sheet contains frames of equal size and they are
+		// all aligned.
+		TextureRegion[][] tmp = TextureRegion.split(spriteSheet, 16, 16);
+
+		// Place the regions into a 1D array in the correct order, starting from the top
+		// left, going across first. The Animation constructor requires a 1D array.
+		TextureRegion[] downFrames = new TextureRegion[2];
+        downFrames[0] = tmp[0][1];
+        downFrames[1] = tmp[0][2];
+
+		TextureRegion[] upFrames = new TextureRegion[2];
+        upFrames[0] = tmp[1][1];
+        upFrames[1] = tmp[1][2];
+
+		TextureRegion[] leftFrames = new TextureRegion[2];
+        leftFrames[0] = tmp[2][2];
+        leftFrames[1] = tmp[3][0];
+
+		TextureRegion[] rightFrames = new TextureRegion[2];
+        rightFrames[0] = tmp[2][0];
+        rightFrames[1] = tmp[2][1];
+
+		TextureRegion[] idleFrames = new TextureRegion[1];
+		idleFrames[0] = tmp[0][0];
+		idleFrames[1] = tmp[1][0];
+		idleFrames[2] = tmp[2][0];
+		idleFrames[3] = tmp[2][2];
+
+
+		// Initialize the Animation with the frame interval and array of frames
+		downAnimation = new Animation<TextureRegion>(.25f, downFrames);
+		upAnimation = new Animation<TextureRegion>(.25f, upFrames);
+		leftAnimation = new Animation<TextureRegion>(.25f, leftFrames);
+		rightAnimation = new Animation<TextureRegion>(.25f, rightFrames);
+		idleAnimation = new Animation<TextureRegion>(.25f, idleFrames);
+
+		// Instantiate a SpriteBatch for drawing and reset the elapsed animation
+		// time to 0
+		spriteBatch = new SpriteBatch();
+		stateTime = 0f;
     }
 
     @Override
     public void render(float delta) {
-        circleX += xSpeed;
-        circleY += ySpeed;
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); // Clear screen
+		stateTime += Gdx.graphics.getDeltaTime(); // Accumulate elapsed animation time
 
-        if (circleX < 0 || circleX > Gdx.graphics.getWidth()) {
-            xSpeed *= -1;
-        }
+		// Get player input
+		playerIdle = true;
+		if(Gdx.input.isKeyPressed(Keys.A)) {
+			playerX -= Gdx.graphics.getDeltaTime() * playerSpeed;
+			playerIdle = false;
+			playerIdleFrame = 3;
+			currentFrame = leftAnimation.getKeyFrame(stateTime, true);
+		}
+	 	if(Gdx.input.isKeyPressed(Keys.D)) {
+			playerX += Gdx.graphics.getDeltaTime() * playerSpeed;
+			playerIdle = false;
+			playerIdleFrame = 2;
+			currentFrame = rightAnimation.getKeyFrame(stateTime, true);
+		}
+		if(Gdx.input.isKeyPressed(Keys.W)) {
+			playerY += Gdx.graphics.getDeltaTime() * playerSpeed;
+			playerIdle = false;
+			playerIdleFrame = 1;
+			currentFrame = upAnimation.getKeyFrame(stateTime, true);
+		}
+	 	if(Gdx.input.isKeyPressed(Keys.S)) {
+			playerY -= Gdx.graphics.getDeltaTime() * playerSpeed;
+			playerIdle = false;
+			playerIdleFrame = 0;
+			currentFrame = downAnimation.getKeyFrame(stateTime, true);
+		}
+  
+		if(playerIdle){
+			currentFrame = idleAnimation.getKeyFrames()[(int)playerIdleFrame];
+		}
 
-        if (circleY < 0 || circleY > Gdx.graphics.getHeight()) {
-            ySpeed *= -1;
-        }
+		spriteBatch.begin();
+		spriteBatch.draw(currentFrame, playerX, playerY, 128, 128); // Draw player
 
-        Gdx.gl.glClearColor(0, 0, .25f, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        game.shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-        game.shapeRenderer.setColor(0, 1, 0, 1);
-        game.shapeRenderer.circle(circleX, circleY, 75);
-        game.shapeRenderer.end();
+		spriteBatch.end();
+
+    
 
     }
 
     @Override
     public void hide() {
-        Gdx.input.setInputProcessor(null);
+        spriteBatch.dispose();
+		spriteSheet.dispose();
     }
 }

dbd92103f661bfcf067485fc78e1334399d38345
Zevin Burzynski <86094612+zevinDev@users.noreply.github.com>
2023-12-12 11:51:16 -0700

    (fix) map

--- core/src/com/fbla/game/GameScreen.java
+++ core/src/com/fbla/game/GameScreen.java
@@ -2,20 +2,11 @@ package com.fbla.game;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
-import com.badlogic.gdx.assets.loaders.resolvers.InternalFileHandleResolver;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.Animation;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.maps.tiled.TideMapLoader;
-import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
-import com.badlogic.gdx.maps.tiled.TmxMapLoader;
-import com.badlogic.gdx.maps.tiled.renderers.BatchTiledMapRenderer;
-import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
@@ -32,22 +23,14 @@ public class GameScreen extends ScreenAdapter {
 
 
 	// Objects used
-	Animation<TextureRegion> npcAnimation; // Must declare frame type (TextureRegion)
 	Animation<TextureRegion> downAnimation; // Must declare frame type (TextureRegion)
 	Animation<TextureRegion> idleAnimation; // Must declare frame type (TextureRegion)
 	Animation<TextureRegion> upAnimation; // Must declare frame type (TextureRegion)
 	Animation<TextureRegion> leftAnimation; // Must declare frame type (TextureRegion)
 	Animation<TextureRegion> rightAnimation; // Must declare frame type (TextureRegion)
 	Texture spriteSheet;
-	Texture gasStation;
-	Texture npc; 
-	private OrthographicCamera cam;
 	SpriteBatch spriteBatch;
 	TextureRegion currentFrame;
-    TiledMapRenderer tiledMapRenderer;
-    Texture img;
-    TiledMap tiledMap;
-    OrthographicCamera camera;
 
 	// A variable for tracking elapsed time for the animation
 	float stateTime;
@@ -59,6 +42,10 @@ public class GameScreen extends ScreenAdapter {
 	boolean playerIdle = true;
 	float playerIdleFrame = 0;
 
+	TiledMapRenderer tiledMapRenderer;
+    TiledMap tiledMap;
+    OrthographicCamera camera;
+
     public GameScreen(FBLA game) {
         this.game = game;
     }
@@ -66,24 +53,13 @@ public class GameScreen extends ScreenAdapter {
 
     @Override
     public void show() {
-		// Constructs a new OrthographicCamera, using the given viewport width and height
-		// Height is multiplied by aspect ratio.
-		cam = new OrthographicCamera(96, 128);
-		cam.update();
-
         // Load the sprite sheet as a Texture
-		spriteSheet = new Texture(Gdx.files.internal("tyler.png"));
-		npc = new Texture(Gdx.files.internal("tyler.png"));
+		spriteSheet = new Texture(Gdx.files.internal("zevin.png"));
 
 		// Use the split utility method to create a 2D array of TextureRegions. This is
 		// possible because this sprite sheet contains frames of equal size and they are
 		// all aligned.
 		TextureRegion[][] tmp = TextureRegion.split(spriteSheet, 16, 16);
-		TextureRegion[][] npcSpritesheet = TextureRegion.split(npc, 16, 16); 
-
-		TextureRegion[] npcIdle =  new TextureRegion[2];
-		npcIdle[0] = npcSpritesheet[2][2];
-		npcIdle[1] = npcSpritesheet[3][0];
 
 		// Place the regions into a 1D array in the correct order, starting from the top
 		// left, going across first. The Animation constructor requires a 1D array.
@@ -110,7 +86,6 @@ public class GameScreen extends ScreenAdapter {
 		idleFrames[3] = tmp[2][2];
 
 
-		npcAnimation = new Animation<TextureRegion>(.25f, npcIdle);
 		// Initialize the Animation with the frame interval and array of frames
 		downAnimation = new Animation<TextureRegion>(.25f, downFrames);
 		upAnimation = new Animation<TextureRegion>(.25f, upFrames);
@@ -118,17 +93,16 @@ public class GameScreen extends ScreenAdapter {
 		rightAnimation = new Animation<TextureRegion>(.25f, rightFrames);
 		idleAnimation = new Animation<TextureRegion>(.25f, idleFrames);
 
-        float w = Gdx.graphics.getWidth();
+		// Instantiate a SpriteBatch for drawing and reset the elapsed animation
+		// time to 0
+		float w = Gdx.graphics.getWidth();
         float h = Gdx.graphics.getHeight();
-
         camera = new OrthographicCamera();
         camera.setToOrtho(false,w,h);
         camera.update();
-        tiledMap = new TmxMapLoader().load("Map01.tmx");
+        tiledMap = new TmxMapLoader().load("new.tmx");
         tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap, 8);
 
-		// Instantiate a SpriteBatch for drawing and reset the elapsed animation
-		// time to 0
 		spriteBatch = new SpriteBatch();
 		stateTime = 0f;
     }
@@ -137,46 +111,30 @@ public class GameScreen extends ScreenAdapter {
 	// If code is innefiecent game will lag (for loop > if statement)
     @Override
     public void render(float delta) {
-        camera.position.set(playerX, playerY, 0);
-        camera.update();
-        tiledMapRenderer.setView(camera);
-        tiledMapRenderer.render(); // Clear screen
+
 		stateTime += Gdx.graphics.getDeltaTime(); // Accumulate elapsed animation time
-		cam.position.set(playerX, playerY, 0);	
-		cam.update();
 
-		handleInput();
-		drawScene();
-    }
-
-    @Override
-    public void hide() {
-        spriteBatch.dispose();
-		spriteSheet.dispose();
-    }
-
-	private void handleInput(){
 		// Get player input
 		playerIdle = true;
-		if(Gdx.input.isKeyPressed(Keys.LEFT)) {
+		if(Gdx.input.isKeyPressed(Keys.A)) {
 			playerX -= Gdx.graphics.getDeltaTime() * (playerSpeed + 25);
 			playerIdle = false;
 			playerIdleFrame = 3;
 			currentFrame = leftAnimation.getKeyFrame(stateTime, true);
 		}
-	 	if(Gdx.input.isKeyPressed(Keys.RIGHT)) {
+	 	if(Gdx.input.isKeyPressed(Keys.D)) {
 			playerX += Gdx.graphics.getDeltaTime() * (playerSpeed + 25);
 			playerIdle = false;
 			playerIdleFrame = 2;
 			currentFrame = rightAnimation.getKeyFrame(stateTime, true);
 		}
-		if(Gdx.input.isKeyPressed(Keys.UP)) {
+		if(Gdx.input.isKeyPressed(Keys.W)) {
 			playerY += Gdx.graphics.getDeltaTime() * playerSpeed;
 			playerIdle = false;
 			playerIdleFrame = 1;
 			currentFrame = upAnimation.getKeyFrame(stateTime, true);
 		}
-	 	if(Gdx.input.isKeyPressed(Keys.DOWN)) {
+	 	if(Gdx.input.isKeyPressed(Keys.S)) {
 			playerY -= Gdx.graphics.getDeltaTime() * playerSpeed;
 			playerIdle = false;
 			playerIdleFrame = 0;
@@ -186,12 +144,23 @@ public class GameScreen extends ScreenAdapter {
 		if(playerIdle){
 			currentFrame = idleAnimation.getKeyFrames()[(int)playerIdleFrame];
 		}
-	}
+		
+		camera.position.set(playerX, playerY, 0);
+        camera.update();
+        tiledMapRenderer.setView(camera);
+        tiledMapRenderer.render(); // Clear screen
 
-	private void drawScene(){
 		spriteBatch.begin();
-		spriteBatch.draw(npcAnimation.getKeyFrame(stateTime, true), 496, 129, 128, 128);
 		spriteBatch.draw(currentFrame, playerX, playerY, 128, 128); // Draw player
+
 		spriteBatch.end();
-	}
+
+
+    }
+
+    @Override
+    public void hide() {
+        spriteBatch.dispose();
+		spriteSheet.dispose();
+    }
 }

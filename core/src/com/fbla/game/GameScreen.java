f55dd9a97733968ce5ad93061408ec6d96ad653e
Zevin Burzynski <86094612+zevinDev@users.noreply.github.com>
2024-01-03 17:21:30 -0800

    (feat) player collision

--- core/src/com/fbla/game/GameScreen.java
+++ core/src/com/fbla/game/GameScreen.java
@@ -3,7 +3,6 @@ package com.fbla.game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Application;
 import com.badlogic.gdx.ScreenAdapter;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.Animation;
@@ -11,64 +10,155 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
-import com.badlogic.gdx.maps.MapObjects;
-import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.maps.MapGroupLayer;
-
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer.Cell;
 
 public class GameScreen extends ScreenAdapter {
     FBLA game;
 
-
-	// Objects used
-	Animation<TextureRegion> downAnimation; // Must declare frame type (TextureRegion)
-	Animation<TextureRegion> idleAnimation; // Must declare frame type (TextureRegion)
-	Animation<TextureRegion> upAnimation; // Must declare frame type (TextureRegion)
-	Animation<TextureRegion> leftAnimation; // Must declare frame type (TextureRegion)
-	Animation<TextureRegion> rightAnimation; // Must declare frame type (TextureRegion)
+	Animation<TextureRegion> downAnimation;
+	Animation<TextureRegion> idleAnimation;
+	Animation<TextureRegion> upAnimation;
+	Animation<TextureRegion> leftAnimation;
+	Animation<TextureRegion> rightAnimation;
 	Texture spriteSheet;
 	SpriteBatch spriteBatch;
 	TextureRegion currentFrame;
 
-	// A variable for tracking elapsed time for the animation
 	float stateTime;
 
-	// Character X and Y coords
 	float playerX = 100;
 	float playerY = 100;
+	float oldX = 100;
+	float oldY = 100;
 	float playerSpeed = 300;
-	boolean playerIdle = true;
 	float playerIdleFrame = 0;
 
-	float lerp = 0.1f;
 
 	TiledMapRenderer tiledMapRenderer;
     TiledMap tiledMap;
     OrthographicCamera cam;
-
+	TiledMapTileLayer collisionLayer;
+	TiledMapTileLayer buildingsLayer;
+	int[] bottomLayers = {0};
+	int[] topLayers = {1};
 
     public GameScreen(FBLA game) {
         this.game = game;
 		Gdx.app.setLogLevel(Application.LOG_DEBUG);
     }
 
-
     @Override
     public void show() {
-        // Load the sprite sheet as a Texture
+		setupAnimations();
+		setupCamera();
+		setupTileMap();
+
+		spriteBatch = new SpriteBatch();
+		stateTime = 0f;
+    }
+
+    @Override
+    public void render(float delta) {
+		stateTime += Gdx.graphics.getDeltaTime();
+		playerMovement();
+		handleCollision();
+    }
+
+    @Override
+    public void hide() {
+        spriteBatch.dispose();
+		spriteSheet.dispose();
+    }
+
+	private void handleCollision(){
+		if(checkCollision(playerX, playerY) == "blocked"){
+			playerY = oldY;
+			playerX = oldX;
+			buildingsLayer.setOpacity(1f);
+			renderScene(oldX, oldY);
+		}else if(checkCollision(playerX, playerY) == "opaque"){
+			buildingsLayer.setOpacity(.25f);
+			renderScene(playerX, playerY);
+		}else {
+			buildingsLayer.setOpacity(1f);
+			renderScene(playerX, playerY);
+		}
+	}
+
+	private void renderScene(float playX, float playY){
+		cam.position.set(playX, playY, 0);
+		cam.update();
+		tiledMapRenderer.setView(cam);
+		tiledMapRenderer.render(bottomLayers);
+		spriteBatch.setProjectionMatrix(cam.combined);
+		spriteBatch.begin();
+		spriteBatch.draw(currentFrame, (playX - 64), (playY - 64), 128, 128);
+		spriteBatch.end();
+		tiledMapRenderer.render(topLayers);
+
+	}
+
+	private String checkCollision(float x, float y) {
+		Cell topRightCell = null;
+		Cell topLeftCell = null;
+		Cell bottomLeftCell = null;
+		Cell bottomRightCell = null;
+	
+		topRightCell = collisionLayer.getCell((int) ((x+64) / 128), (int) (y / 128));
+		topLeftCell = collisionLayer.getCell((int) ((x-64) / 128), (int) (y / 128));
+		bottomLeftCell = collisionLayer.getCell((int) ((x-64) / 128), (int) ((y-64) / 128));
+		bottomRightCell = collisionLayer.getCell((int) ((x+64) / 128), (int) ((y-64) / 128));
+
+		return checkCellCollision(topRightCell, topLeftCell, bottomLeftCell, bottomRightCell);
+	}
+
+	private String checkCellCollision(Cell... cells){
+		for(Cell cell : cells){
+			if (cell != null && cell.getTile() != null) {
+				if (cell.getTile().getProperties().containsKey("blocked")) {
+					return "blocked";
+				} else if (cell.getTile().getProperties().containsKey("opaque")) {
+					return "opaque";
+				}
+			}
+		}
+		return "nothing";
+	}
+
+	private void playerMovement(){
+		oldX = playerX;
+		oldY = playerY;
+		if(Gdx.input.isKeyPressed(Keys.A)) {
+			playerX -= Gdx.graphics.getDeltaTime() * (playerSpeed + 25);
+			playerIdleFrame = 3;
+			currentFrame = leftAnimation.getKeyFrame(stateTime, true);
+		}
+	 	else if(Gdx.input.isKeyPressed(Keys.D)) {
+			playerX += Gdx.graphics.getDeltaTime() * (playerSpeed + 25);
+			playerIdleFrame = 2;
+			currentFrame = rightAnimation.getKeyFrame(stateTime, true);
+		}
+		else if(Gdx.input.isKeyPressed(Keys.W)) {
+			playerY += Gdx.graphics.getDeltaTime() * playerSpeed;
+			playerIdleFrame = 1;
+			currentFrame = upAnimation.getKeyFrame(stateTime, true);
+		}
+	 	else if(Gdx.input.isKeyPressed(Keys.S)) {
+			playerY -= Gdx.graphics.getDeltaTime() * playerSpeed;
+			playerIdleFrame = 0;
+			currentFrame = downAnimation.getKeyFrame(stateTime, true);
+		} else {
+			currentFrame = idleAnimation.getKeyFrames()[(int)playerIdleFrame];
+		}
+	}
+
+	private void setupAnimations(){
 		spriteSheet = new Texture(Gdx.files.internal("zevin.png"));
-
-		// Use the split utility method to create a 2D array of TextureRegions. This is
-		// possible because this sprite sheet contains frames of equal size and they are
-		// all aligned.
 		TextureRegion[][] tmp = TextureRegion.split(spriteSheet, 16, 16);
-
-		// Place the regions into a 1D array in the correct order, starting from the top
-		// left, going across first. The Animation constructor requires a 1D array.
 		TextureRegion[] downFrames = new TextureRegion[2];
         downFrames[0] = tmp[0][1];
         downFrames[1] = tmp[0][2];
@@ -91,80 +181,24 @@ public class GameScreen extends ScreenAdapter {
 		idleFrames[2] = tmp[2][0];
 		idleFrames[3] = tmp[2][2];
 
-
-		// Initialize the Animation with the frame interval and array of frames
 		downAnimation = new Animation<TextureRegion>(.25f, downFrames);
 		upAnimation = new Animation<TextureRegion>(.25f, upFrames);
 		leftAnimation = new Animation<TextureRegion>(.25f, leftFrames);
 		rightAnimation = new Animation<TextureRegion>(.25f, rightFrames);
 		idleAnimation = new Animation<TextureRegion>(.25f, idleFrames);
+	}
 
-		// Instantiate a SpriteBatch for drawing and reset the elapsed animation
-		// time to 0
+	private void setupCamera(){
 		cam = new OrthographicCamera(); 
 		cam.setToOrtho(false);
 		cam.position.set(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2, 0); 
-		cam.update(); // Updates the camera
-        tiledMap = new TmxMapLoader().load("new.tmx");
+		cam.update();
+	}
+	
+	private void setupTileMap(){
+        tiledMap = new TmxMapLoader().load("map.tmx");
         tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap, 8);
-		spriteBatch = new SpriteBatch();
-		stateTime = 0f;
-    }
-
-	// This method runs for every frame
-	// If code is innefiecent game will lag (for loop > if statement)
-    @Override
-    public void render(float delta) {
-
-		stateTime += Gdx.graphics.getDeltaTime(); // Accumulate elapsed animation time
-		// Get player input
-		playerIdle = true;
-		if(Gdx.input.isKeyPressed(Keys.A)) {
-			playerX -= Gdx.graphics.getDeltaTime() * (playerSpeed + 25);
-			playerIdle = false;
-			playerIdleFrame = 3;
-			currentFrame = leftAnimation.getKeyFrame(stateTime, true);
-		}
-	 	else if(Gdx.input.isKeyPressed(Keys.D)) {
-			playerX += Gdx.graphics.getDeltaTime() * (playerSpeed + 25);
-			playerIdle = false;
-			playerIdleFrame = 2;
-			currentFrame = rightAnimation.getKeyFrame(stateTime, true);
-		}
-		else if(Gdx.input.isKeyPressed(Keys.W)) {
-			playerY += Gdx.graphics.getDeltaTime() * playerSpeed;
-			playerIdle = false;
-			playerIdleFrame = 1;
-			currentFrame = upAnimation.getKeyFrame(stateTime, true);
-		}
-	 	else if(Gdx.input.isKeyPressed(Keys.S)) {
-			playerY -= Gdx.graphics.getDeltaTime() * playerSpeed;
-			playerIdle = false;
-			playerIdleFrame = 0;
-			currentFrame = downAnimation.getKeyFrame(stateTime, true);
-		}
-  
-		if(playerIdle){
-			currentFrame = idleAnimation.getKeyFrames()[(int)playerIdleFrame];
-		}
-
-
-
-		cam.position.set(playerX, playerY, 0); // x and y could be changed by Keyboard input for example
-		cam.update(); // Don't forget me ;)
-        tiledMapRenderer.setView(cam);
-        tiledMapRenderer.render(); // Clear screen
-		spriteBatch.setProjectionMatrix(cam.combined); // Tells the spritebatch to render according to your camera
-		spriteBatch.begin();
-		spriteBatch.draw(currentFrame, (playerX - 64), (playerY - 64), 128, 128); // Draw player
-		spriteBatch.end();
-
-
-    }
-
-    @Override
-    public void hide() {
-        spriteBatch.dispose();
-		spriteSheet.dispose();
-    }
+		collisionLayer = (TiledMapTileLayer)tiledMap.getLayers().get("Collision");
+		buildingsLayer = (TiledMapTileLayer)tiledMap.getLayers().get("Buildings");
+	}
 }

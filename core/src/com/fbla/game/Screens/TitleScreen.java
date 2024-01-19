e920c6eead529a5603de12b1b6258ef90e843a03
Zevin Burzynski <86094612+zevinDev@users.noreply.github.com>
2024-01-18 19:46:06 -0700

    (feat) titlescreen

--- core/src/com/fbla/game/Screens/TitleScreen.java
+++ core/src/com/fbla/game/Screens/TitleScreen.java
@@ -6,11 +6,21 @@ import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.fbla.game.FBLA;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.fbla.game.Util.GifDecoderUtil;
+import com.badlogic.gdx.graphics.Texture;
 
 
 public class TitleScreen extends ScreenAdapter {
 
     FBLA game;
+    Animation<TextureRegion> animation;
+    float elapsed;
+    TextureRegion[][] startSheet;
+    TextureRegion currentFrame;
+    Texture settingsButton;
+    Texture leaderboardButton;
 
     public TitleScreen(FBLA game) {
         this.game = game;
@@ -18,30 +28,42 @@ public class TitleScreen extends ScreenAdapter {
 
     @Override
     public void show(){
-        Gdx.input.setInputProcessor(new InputAdapter() {
-            @Override
-            public boolean keyDown(int keyCode) {
-                if (keyCode == Input.Keys.SPACE) {
-                    game.setScreen(new GameScreen(game));
-                }
-                return true;
-            }
-        });
+        animation = GifDecoderUtil.loadGIFAnimation(Animation.PlayMode.LOOP, Gdx.files.internal("spritesheets/titlescreen.gif").read());
+        startSheet = TextureRegion.split(new Texture(Gdx.files.internal("spritesheets/start.png")), 720, 208);
+        settingsButton = new Texture(Gdx.files.internal("spritesheets/settings.png"));
+        leaderboardButton = new Texture(Gdx.files.internal("spritesheets/leaderboard.png"));
     }
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(0, .25f, 0, 1);
+        elapsed += Gdx.graphics.getDeltaTime();
+        
+        Gdx.gl.glClearColor(38/255f, 43/255f, 68/255f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         game.batch.begin();
-        game.font.draw(game.batch, "Title Screen!", Gdx.graphics.getWidth() * .25f, Gdx.graphics.getHeight() * .75f);
-        game.font.draw(game.batch, "Click the circle to win.", Gdx.graphics.getWidth() * .25f, Gdx.graphics.getHeight() * .5f);
-        game.font.draw(game.batch, "Press space to play.", Gdx.graphics.getWidth() * .25f, Gdx.graphics.getHeight() * .25f);
+        game.batch.draw(animation.getKeyFrame(elapsed), 64, 704);
+        
+        // Get the mouse's x and y coordinates
+        int mouseX = Gdx.input.getX();
+        int mouseY = Gdx.input.getY();
+    
+        // Check if the mouse is over the start button
+        if (mouseX >= 396 && mouseX <= 729 && mouseY >= 726 && mouseY <= 830) {
+            // If the mouse is over the start button, change the current frame
+            currentFrame = startSheet[1][0]; // Change this to the frame you want
+            if (Gdx.input.isButtonPressed(Input.Buttons.LEFT)) {
+                game.setScreen(new GameScreen(game));
+            }
+        } else {
+            currentFrame = startSheet[0][0]; // Change this to the default frame
+        }
+        game.batch.draw(leaderboardButton, 64, 64);
+        game.batch.draw(settingsButton, 1152 - (64 + 128), 64);
+        game.batch.draw(currentFrame, 396, 450, 360, 104);
         game.batch.end();
     }
 
     @Override
     public void hide(){
-        Gdx.input.setInputProcessor(null);
     }
 }
